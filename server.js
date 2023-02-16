498f7179de67dcd2dc3b4ff4826fd91e0be3425f
hyunjun <guswns212000@gmail.com>
2023-02-16 20:55:13 +0900

    0216_1

--- server.js
+++ server.js
@@ -14,18 +14,20 @@ mongoose.connect("mongodb+srv://KHJ:rla0407@cluster0.gnyfswz.mongodb.net/?retryW
   .catch(err => (console.log(err)))
 
 
-app.get("/", (req,res) => {res.send("안녕")});
+// app.get("/", (req,res) => {res.send("안녕")});
 
-app.post("/signup",(req, res) => {
-    //회원가입 시 필요한 정보를 클라이언트에서 가져와서 데이터베이스에 넣어줌
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-        if(err) return res.json({ success: false, err})
-        return res.status(200).json({
-            success: true
+function userSignup(root, myform){
+    app.post(root,(myform, res) => {
+        //회원가입 시 필요한 정보를 클라이언트에서 가져와서 데이터베이스에 넣어줌
+        const user = new User(myform)
+        user.save((err, userInfo) => {
+            if(err) return res.json({ success: false, err})
+            return res.status(200).json({
+                success: true
+            })
         })
     })
-})
-
+}
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
+
+module.exports = {userSignup};
