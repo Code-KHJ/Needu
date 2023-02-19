9a7d3fcfd39701e889849c945c14d6af3e6954c9
hyunjun <guswns212000@gmail.com>
2023-02-19 22:24:42 +0900

    0219 db연동

--- server.js
+++ server.js
@@ -1,31 +1,46 @@
 const express = require("express");
 const app = express();
-const port = 3010;
+const port = 3000;
 const bodyParser = require("body-parser");
-const { User } = require("./models/user");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://KHJ:rla0407@cluster0.gnyfswz.mongodb.net/?retryWrites=true&w=majority", {
-}).then(() => console.log("MongoDB Connected..."))
-  .catch(err => (console.log(err)))
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'rla0407!',
+    database: 'recruit-sw'
+    
+})
+connection.connect(function(err) {
+   if(err) throw err;
+   console.log('mysql Connected') 
+});
 
 
 app.get("/", (req,res) => {res.send("안녕")});
 
 app.post("/signup",(req, res) => {
     //회원가입 시 필요한 정보를 클라이언트에서 가져와서 데이터베이스에 넣어줌
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
+    const sqlinsert = 'INSERT INTO user SET ?';
+    const user_info = {
+        id : req.body.id,
+        password : req.body.password,
+        name : req.body.name,
+        phonenumber : req.body.phonenumber,
+        email : req.body.email
+    }
+    connection.query(sqlinsert, user_info, function(err, rows, fields){
         if(err) return res.json({ success: false, err})
         return res.status(200).json({
-            success: true
+            success: true     
+        })
         })
     })
-})
+    
+// connection.end()
+
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
