09494d184d420d37acf9bef1e040f0065c779b23
hyunjun <guswns212000@gmail.com>
2023-03-29 02:24:57 +0900

    회원가입수정

--- routes/controllers/user.js
+++ routes/controllers/user.js
@@ -65,11 +65,10 @@ module.exports = {
     },
     signup: (req,res) => {
         checkbody = [
-        body('id').trim().notEmpty().isLength({min:4, max:20}).isAlphanumeric().withMessage("아이디 오류"),
+        body('id').trim().notEmpty().isEmail().withMessage("아이디 오류"),
         body('password').trim().notEmpty().isLength({min:8, max:16}).withMessage("비밀번호 오류"),    
-        body('name').trim().notEmpty().isLength({min:1}).withMessage("이름 오류"),
         body('phonenumber').trim().notEmpty().isMobilePhone('any').withMessage("휴대폰번호 오류"),
-        body('email').trim().notEmpty().isEmail().withMessage("이메일 오류")
+        body('nickname').trim().notEmpty().isLength({min:2}).withMessage("닉네임 오류"),
         ]
         const err = validationResult(checkbody);
         if (!err.isEmpty()){
@@ -78,23 +77,20 @@ module.exports = {
         const user_info = {
             id : req.body.id,
             password : req.body.password,
-            name : req.body.name,
             phonenumber : req.body.phonenumber,
-            email : req.body.email,
+            nickname : req.body.nickname,
             required_check1 : Boolean(req.body.check_2),
             required_check2 : Boolean(req.body.check_3),
             optional_check1 : Boolean(req.body.check_4),
             optional_check2 : Boolean(req.body.check_5),
             info_period : req.body.radio1,
         }
-        console.log(req.body)
-        console.log(user_info)
         pool.query('SELECT id FROM user WHERE id = "' + user_info.id + '"', (err, row)=>{
             if (row[0] == undefined){ //동일한 아이디가 없을 경우
                 const salt = bcrypt.genSaltSync(saltRounds);
                 const hashPw = bcrypt.hashSync(user_info.password, salt);
-                pool.query('insert into user (id, password, name, phonenumber, email, required_check1, required_check2, optional_check1, optional_check2, info_period) values (?,?,?,?,?,?,?,?,?,?)',
-                    [user_info.id, hashPw, user_info.name, user_info.phonenumber, user_info.email, user_info.required_check1,
+                pool.query('insert into user (id, password, phonenumber, nickname, required_check1, required_check2, optional_check1, optional_check2, info_period) values (?,?,?,?,?,?,?,?,?)',
+                    [user_info.id, hashPw, user_info.phonenumber, user_info.nickname, user_info.required_check1,
                      user_info.required_check2, user_info.optional_check1, user_info.optional_check2, user_info.info_period], (err, rows, fields)=>{
                     if(err) return res.json({ success: false, err})
                     return res.status(200).send("<script>alert('회원가입이 완료되었습니다.');location.href = '/login';</script>");
@@ -105,16 +101,29 @@ module.exports = {
             }
         })
     },
-    checkId: (req,res) => {
-        const checkId = req.body.id
+    duplic: (req,res) => {
+        const check = req.body.check
+        const checkId = req.body.checkId
+        const checkName = req.body.checkName
         let result = 1
-        pool.query('SELECT id FROM user WHERE id = "' + checkId + '"', (err, row)=>{
-            if(row[0] == undefined){
-                res.send(JSON.stringify(result))
-            } else{
-                result = 2
-                res.send(JSON.stringify(result))
-            }
-        })
+        if(check == "id"){
+            pool.query('SELECT id FROM user WHERE id = "' + checkId + '"', (err, row)=>{
+                if(row[0] == undefined){
+                    res.send(JSON.stringify(result))
+                } else{
+                    result = 2
+                    res.send(JSON.stringify(result))
+                }
+        })} else{
+            if(check == "nickname"){
+                pool.query('SELECT nickname FROM user WHERE nickname = "' + checkName + '"', (err, row)=>{
+                    if(row[0] == undefined){
+                        res.send(JSON.stringify(result))
+                    } else{
+                        result = 2
+                        res.send(JSON.stringify(result))
+                    }
+            })}    
+        }
     }
 }
