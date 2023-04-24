b00262dab8c8a3dc1386a927decd25b768cdb9b1
hyunjun <guswns212000@gmail.com>
2023-04-24 23:17:17 +0900

    기관명 db연동, 페이지 연동

--- routes/review.js
+++ routes/review.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const rootdir = require("../modules/path");
-const {auth} = require("./middleware/auth");
 const { write } = require("./controllers/review");
 
 
-router.get('/:name', auth, (req,res)=>{
+router.get('/:name', (req,res)=>{
   const Corp_name = req.params.name
-  res.render(rootdir+'/public/review_write.html', {Corp_name: Corp_name})})
+  res.render(rootdir+'/public/review.html', {Corp_name: Corp_name})})
 
 
-router.post('/:name', auth, write)
+router.post('/', )
 
 module.exports = router;
