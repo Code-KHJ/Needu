ae4ec0d68923ca659f5f06c836905bd7a6bc9252
hyunjun <guswns212000@gmail.com>
2023-06-10 02:48:13 +0900

    수정

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const rootdir = require("./modules/path");
+const rootdir = require("../modules/path");
 const {auth} = require("./middleware/auth");
 const {Corp_info, Hash_info, mid_top10_corp} = require("./middleware/corp");
 const { review_content, review_auth } = require("./middleware/review");
@@ -8,7 +8,7 @@ const { review, review_more, review_likes } = require("./controllers/review");
 
 
 router.get('/', auth, (req,res)=>{
-  res.render(rootdir+'/public/search_review.html', {User: req.user})
+  res.render('search_review.html', {User: req.user})
 })
 
 router.get('/:name', auth, Corp_info, Hash_info, review_content, mid_top10_corp, review)
