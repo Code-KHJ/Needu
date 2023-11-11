fdec74c6e1b2b0e894b3a4487fc298a7dc57eb41
hyunjun <guswns212000@gmail.com>
2023-11-11 15:35:03 +0900

    feat: chat reserve 우피 연동

--- routes/chat.js
+++ routes/chat.js
@@ -4,10 +4,38 @@ const rootdir = require('../modules/path');
 
 const { auth } = require('./middleware/auth');
 
-router.get('/', auth, (req, res) => {
+router.get('/reserve', auth, (req, res) => {
+  const middle_info = {
+    User: req.user,
+    Host: req.query.id,
+  };
+  if (!middle_info.User) {
+    return res
+      .status(401)
+      .send(
+        "<script>alert('로그인 하신 후 이용할 수 있는 서비스입니다.');location.href = '/login';</script>"
+      );
+  }
+  if (!middle_info.Host) {
+    return res
+      .status(404)
+      .send("<script>alert('에러');location.href = '/';</script>");
+  }
+  res.status(200).render(rootdir + '/public/chat_reserve.html', middle_info);
+});
+
+router.get('/complete', auth, (req, res) => {
   const middle_info = {
     User: req.user,
   };
+  if (!middle_info.User) {
+    return res
+      .status(401)
+      .send(
+        "<script>alert('로그인 하신 후 이용할 수 있는 서비스입니다.');location.href = '/login';</script>"
+      );
+  }
+
   res.status(200).render(rootdir + '/public/chat_complete.html', middle_info);
 });
 
